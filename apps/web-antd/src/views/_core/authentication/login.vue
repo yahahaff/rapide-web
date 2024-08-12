91d6fc88c5cc5a9e39c2a4cd6436f45dc10e16d0
yahahaff <yahahaff@qq.com>
2024-08-12 17:52:29 +0800

    refactor:取消用户名密码提示

--- apps/web-antd/src/views/_core/authentication/login.vue
+++ apps/web-antd/src/views/_core/authentication/login.vue
@@ -11,8 +11,8 @@ const authStore = useAuthStore();
 <template>
   <AuthenticationLogin
     :loading="authStore.loginLoading"
-    password-placeholder="123456"
-    username-placeholder="vben"
+    password-placeholder=""
+    username-placeholder=""
     @submit="authStore.authLogin"
   />
 </template>
